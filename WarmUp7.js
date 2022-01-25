1cf27f28a0781ce520928f6390ba6ad55b6367df
NeirouzJbira <jbira.neirouz@gmail.com>
2022-01-25 14:46:23 +0100

    init

---
+++ WarmUp7.js
@@ -0,0 +1,47 @@
+// ======= EACH =================================================================================================================================================
+// Complete the greatestProduct method so that it'll find the greatest product of five consecutive digits in the given string of digits.
+
+// For example:
+
+// greatestProduct("123834539327238239583") // should return 3240
+
+// The input string always has more than five digits.
+
+// ========================================================================================================================================================
+
+// ============== MAP ==========================================================================================================================================
+
+// Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
+// alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.CharCodeAt() and see if you can use
+// Ascii code to acomplish this.
+
+// ex. shiftLetters('hello') // => 'ifmmp'
+
+// ex. (shiftLetters('abcxyz') // => "bcdyz{"
+
+// ========================================================================================================================================================
+
+// ============= FILTER ===========================================================================================================================================
+
+// Your goal in this warmUp is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+// ========================================================================================================================================================
+
+// ============= REDUCE ===========================================================================================================================================
+
+// Write a function that takes a string and returns an object representing the character
+
+// count for each letter. Use .reduce to build this object.
+
+// ex. countLetters('abbcccddddeeeee') // => {a:1, b:2, c:3, d:4, e:5}
+
+// ========================================================================================================================================================
+
+//Good Luck :))
