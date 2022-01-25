5f32aef4a40f38b9ef58d82f7080f72fa8d95448
NeirouzJbira <jbira.neirouz@gmail.com>
2022-01-25 14:46:10 +0100

    init

---
+++ WarmUp1.js
@@ -0,0 +1,14 @@
+// 1-Create a third variable called z, assign x + y to it, and display it.
+
+// 2-calculate the area of a square using varaiables.
+
+// 3-using + operator combine your partner first and last name .
+
+// 4-find if the number 13 is a multiple of 3 or not.
+
+// 5-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+
+// 6-calculate your age in seconds.
+
+// 7-calculate the area of a square using varaiables.
+// /Area of square is length * Width
