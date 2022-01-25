6e1680e1622572487ad06f6a8adbc62cefb922cd
NeirouzJbira <jbira.neirouz@gmail.com>
2022-01-25 14:46:19 +0100

    init

---
+++ WarmUp5.js
@@ -0,0 +1,18 @@
+//1-Using recursion, Write a function that accepts a string and returns the number of vowels in that string.
+//Note:Five of the 26 alphabet letters are vowels: A, E, I, O, and U.
+//countVowels("four score and seven years") ==> 9;
+//countVowels("rbk") ==> 0
+
+//2-implement the function mult that takes a single parameter n, and computes the multiplication of all integers up to n
+//starting from 0, e.g.:
+// mult(3); // => 6
+// mult(4); // => 24
+
+//3-Create a function isSubset that should take two arrays and determine whether the second array is a subset of the first array.
+//For example:
+
+// array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
+// array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
+// array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+// Read carefully, focus and test your code :) ... /\ HaPPy HaCkInG /\
