48ae097f25f0df189678abd129918607c2be35d4
NeirouzJbira <jbira.neirouz@gmail.com>
2022-01-25 14:46:15 +0100

    init

---
+++ WarmUp3.js
@@ -0,0 +1,50 @@
+//1
+// Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str, which is the string to be repeated,
+//and count -- a number representing how many times the string str should be repeated
+// repeatString('dog', 0); // => ''
+// repeatString('dog', 1); // => 'dog'
+// repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog'
+// repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
+
+// // USE RECURSION
+
+//2
+// Write a function named isEven using  for loop that
+// -  iterate from x to y.
+// -  return array contain the even values,
+//  ex:  isEven(1,10) => [2,4,6,8,10]
+
+//3
+// write a function named sum that
+// - Use a while loop to add up the numbers from x to y.
+// ex sum(1,5) => 15
+
+//4
+// Write a function named factorial that
+// -  Use Recursion to calculate the factorial of a number
+// -  the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less
+//than or equal to n
+// - 5! = 5*4*3*2*1 = 120
+// ex : factorial(5) => 120
+
+//5
+// write a function named decimals
+//- the function will format a number up to specified decimal places
+//- the function will return a string
+//- if the parameters not a number return false
+// ex :
+//      decimals(2100, 'a') ==> false
+//      decimals('a', 5) ==> false
+//      decimals(2.100212, 2) ==> '2.10'
+//      decimals(2.100212, 3) ==> '2.100'
+//      decimals(2100, 2) ==> '2100.00'
+//
+
+//6
+// write a function to Reverse an array without use array.reverse() method:
+// ex: reverseArray([1,2,3]) ==> [3,2,1]
+
+//7
+// Write a function called laugh() that takes one parameter,
+// num that represents the number of "ha"s to return.
+// laugh(6); => "hahahahahaha"
